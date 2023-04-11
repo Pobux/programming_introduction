0764358cd619302c474e41cbfcef5d2f2d427f4d
Your Name <you@example.com>
2023-04-11 14:42:25 -0400

    week 11

---
+++ week11/exercices.py
@@ -0,0 +1,14 @@
+# Create a function that add a line in a file
+# each time the users run the script, the line should have
+# The date, the users name and a message
+import datetime #datetime.now() 
+import getpass  #getpass.getuser()
+
+
+# Use the previous function and add an argument called "subfunc"
+# Print the subfunc function name and a message to it.
+# pass "look_at_photo" to the function
+# This is more or less how apps online logs everything you do
+
+def look_at_photo():
+    print("User Madame is looking at the photo of her friend")
